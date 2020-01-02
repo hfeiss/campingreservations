6c43f19566048d49a8a3d0a8b576638f734c9050
hfeiss <hfeiss@gmail.com>
2020-01-02 15:33:17 -0700

    Clean up code in spark.py

--- src/data/columns.py
+++ src/data/columns.py
@@ -2,3 +2,121 @@ lst = ['HistoricalReservationID', 'OrderNumber', 'Agency', 'OrgID', 'CodeHierarc
 
 for work in lst:
     print(work)
+
+HistoricalReservationID
+OrderNumber
+Agency
+OrgID
+CodeHierarchy
+RegionCode
+RegionDescription
+ParentLocationID
+ParentLocation
+LegacyFacilityID
+Park
+SiteType
+UseType
+ProductID
+EntityType
+EntityID
+FacilityID
+FacilityZIP
+FacilityState
+FacilityLongitude
+FacilityLatitude
+CustomerZIP
+CustomerState
+CustomerCountry
+Tax
+UseFee
+TranFee
+AttrFee
+TotalBeforeTax
+TotalPaid
+StartDate
+EndDate
+OrderDate
+NumberOfPeople
+Tent
+Popup
+Trailer
+RVMotorhome
+Boat
+HorseTrailer
+Car
+FifthWheel
+Van
+CanoeKayak
+BoatTrailer
+Motorcycle
+Truck
+Bus
+Bicycle
+Snowmobile
+OffRoadlAllTerrainVehicle
+PowerBoat
+PickupCamper
+LargeTentOver9x12
+SmallTent
+Marinaboat
+LatLongPoint
+
+OrderNumber,
+UseType,
+FacilityID,
+FacilityZIP,
+FacilityLongitude,
+FacilityLatitude,
+CustomerZIP,
+TotalPaid,
+StartDate,
+EndDate,
+OrderDate,
+NumberOfPeople,
+Tent,
+Popup,
+Trailer,
+RVMotorhome,
+Boat,
+Car,
+FifthWheel,
+Van,
+CanoeKayak,
+BoatTrailer,
+PowerBoat,
+PickupCamper,
+LargeTentOver9x12,
+SmallTent
+
+sch = StructType([StructField('OrderNumber', StringType(), True),
+                    StructField('UseType', StringType(), True),
+                    StructField('FacilityID', IntegerType(), True),
+                    StructField('FacilityZIP', IntegerType(), True),
+                    StructField('FacilityLongitude', FloatType(), True),
+                    StructField('FacilityLatitude', FloatType(), True),
+                    StructField('CustomerZIP', IntegerType(), True),
+                    StructField('TotalPaid', FloatType(), True),
+                    StructField('StartDate', DateType(), True),
+                    StructField('EndDate', DateType(), True),
+                    StructField('NumberOfPeople', IntegerType(), True),
+                    StructField('Tent', BooleanType(), True),
+                    StructField('Popup', BooleanType(), True),
+                    StructField('Trailer', BooleanType(), True),
+                    StructField('RVMotorhome', BooleanType(), True),
+                    StructField('Boat', BooleanType(), True),
+                    StructField('Car', BooleanType(), True),
+                    StructField('FifthWheel', BooleanType(), True),
+                    StructField('Van', BooleanType(), True),
+                    StructField('CanoeKayak', BooleanType(), True),
+                    StructField('BoatTrailer', BooleanType(), True),
+                    StructField('PowerBoat', BooleanType(), True),
+                    StructField('PickupCamper', BooleanType(), True),
+                    StructField('LargeTentOver9x12', BooleanType(), True),
+                    StructField('SmallTent', BooleanType(), True)])
+
+
+    def make_LengthOfStay(self):
+        result = self.current.withColumn('LengthOfStay',
+                self.current['EndDate'] - self.current['StartDate'])
+        result.createOrReplaceTempView('temp')
+        self.current = self.to_df()
--- src/data/spark.py
+++ src/data/spark.py
@@ -5,11 +5,13 @@ import datetime
 import os
 from zipcoords import get_lat, get_lng
 
+# Create filepaths within df directory
 srcpath = os.path.split(os.path.abspath(''))[0]
 rootpath = os.path.split(srcpath)[0]
 datapath = os.path.join(rootpath, 'data/')
 rawpath = os.path.join(datapath, 'raw/')
 
+# Start a spark session
 spark = (ps.sql.SparkSession.builder 
         .master("local") 
         .appName("Capstone I")
@@ -19,6 +21,8 @@ sc = spark.sparkContext
 
 get_lat_udf = udf(get_lat, FloatType())
 get_lng_udf = udf(get_lng, FloatType())
+get_len_udf = udf(lambda row: row['EndDate']
+                            - row['StartDate'])
 
 class Data(object):
     """
@@ -36,75 +40,92 @@ class Data(object):
 
     def __init__(self, filename):
         self.filename = filename
-        self.raw = spark.read.csv(filename, header=True, inferSchema=True)
+        self.raw = spark.read.csv(filename,
+                                    header=True,
+                                    inferSchema=True)
         self.raw.createOrReplaceTempView('temp')
-        self.current = self.to_df()
+        self.df = self.to_df()
 
     # Use this to create & return a spark.df from the temp
     # For spark methods outside of this class
     def to_df(self):
-        result = spark.sql('''
+        return spark.sql('''
                         SELECT
                             *
                         FROM
                             temp
-                        ''')        
-        return result      
+                        ''')    
 
     def select_columns(self):
         result = spark.sql('''
-                            SELECT
-                                STRING(OrderNumber),
-                                STRING(UseType),
-                                INT(FacilityID),
-                                FacilityLongitude,
-                                FacilityLatitude,
-                                CustomerZIP,
-                                TotalPaid,
-                                StartDate,
-                                EndDate,
-                                INT(NumberOfPeople),
-                                INT(Tent),
-                                INT(Popup),
-                                INT(Trailer),
-                                INT(RVMotorhome),
-                                INT(Boat),
-                                INT(Car),
-                                INT(FifthWheel),
-                                INT(Van),
-                                INT(CanoeKayak),
-                                INT(BoatTrailer),
-                                INT(PowerBoat),
-                                INT(PickupCamper),
-                                INT(LargeTentOver9x12),
-                                INT(SmallTent)
-                            FROM
-                                temp
-                            ''')
+                    SELECT
+                        STRING(OrderNumber),
+                        STRING(UseType),
+                        INT(FacilityID),
+                        FacilityLongitude,
+                        FacilityLatitude,
+                        CustomerZIP,
+                        TotalPaid,
+                        StartDate,
+                        EndDate,
+                        INT(NumberOfPeople),
+                        INT(Tent),
+                        INT(Popup),
+                        INT(Trailer),
+                        INT(RVMotorhome),
+                        INT(Boat),
+                        INT(Car),
+                        INT(FifthWheel),
+                        INT(Van),
+                        INT(CanoeKayak),
+                        INT(BoatTrailer),
+                        INT(PowerBoat),
+                        INT(PickupCamper),
+                        INT(LargeTentOver9x12),
+                        INT(SmallTent)
+                    FROM
+                        temp
+                    ''')
         result.createOrReplaceTempView('temp')
-        self.current = self.to_df()
-
-
-    def make_customer_coords(self):
-        # need to deal with long zipcodes...
-        result = self.current.withColumn('CustomerLatitude',
-                        get_lat_udf(self.current['CustomerZIP']))\
-                    .withColumn('CustomerLongitude',
-                        get_lng_udf(self.current['CustomerZIP']))
-        result.createOrReplaceTempView('temp')
-        self.current = self.to_df
+        self.df = self.to_df()
 
     def remove_nulls(self):
-        result = self.current.dropna(how = 'any', subset = ['OrderNumber', 'FacilityID', 
-            'FacilityLongitude', 'FacilityLatitude', 'CustomerZIP', 'StartDate',
+        result = self.df.dropna(how = 'any', subset = 
+            ['OrderNumber',
+            'FacilityID', 
+            'FacilityLongitude',
+            'FacilityLatitude',
+            'CustomerZIP',
+            'StartDate',
             'EndDate'])
         result.createOrReplaceTempView('temp')
-        self.current = self.to_df()
+        self.df = self.to_df()
+
+    def make_CustomerLatitude(self):
+        result = self.df.withColumn('CustomerLatitude',
+                    get_lat_udf(self.df['CustomerZIP']))
+        result.createOrReplaceTempView('temp')
+        self.df = self.to_df()
+
+    def make_CustomerLongitude(self):
+        result = self.df.withColumn('CustomerLongitude',
+                    get_lng_udf(self.df['CustomerZIP']))
+        result.createOrReplaceTempView('temp')
+        self.df = self.to_df()
+
+    def make_LengthOfStay(self):
+        result = self.df.withColumn('LengthOfStay',
+                    get_len_udf(self.df))
+        result.createOrReplaceTempView('temp')
+        self.df = self.to_df()
 
     def clean(self):
         self.select_columns()
         self.remove_nulls()
-        self.make_customer_coords()        
+        self.make_CustomerLatitude()
+        self.make_CustomerLongitude()     
+        self.make_LengthOfStay()   
+        
 
     def combine(self):
         pass
