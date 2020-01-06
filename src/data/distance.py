e62f46e2ddd6efc1d7307fdeddfe50254c93c9ca
hfeiss <hfeiss@gmail.com>
2020-01-06 08:29:20 -0700

    Rename distance.py and add get_dst

--- src/data/distance.py
+++ src/data/distance.py
@@ -1,4 +1,5 @@
 from uszipcode import SearchEngine
+import geopy.distance as dist
 
 search = SearchEngine(simple_zipcode=True)
 
@@ -21,6 +22,16 @@ def get_lng(zipcode):
     zip_dct = zip_obj.to_dict()
     return zip_dct['lng']
    
+def get_dst(lat_one, lng_one, lat_two, lng_two):
+    coords_one = (lat_one, lng_one)
+    coords_two = (lat_two, lng_two)
+    return dist.distance(coords_one, coords_two).miles
 
 if __name__ == '__main__':
-    print(zipcode_to_coords(59601))
+    helena = zipcode_to_coords('59601-0112')
+    portland = zipcode_to_coords(97211)
+    print(f'Helena is at {helena}')
+    print(f'Porltand is at {portland}')
+    distance = get_dst(helena[0], helena[1],
+                    portland[0], portland[1])
+    print(f'The distance between the two is {distance:.2f} miles')
