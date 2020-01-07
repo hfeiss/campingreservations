6083d5e0e969667c645425ec1702dc3ff4dafc0e
hfeiss <hfeiss@gmail.com>
2020-01-07 11:33:48 -0700

    Tweak categories visualization

--- src/visualizations/categories.py
+++ src/visualizations/categories.py
@@ -55,8 +55,7 @@ for file in list_years:
 
 data.reset_index(inplace = True)
 
-tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
-             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
+tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (44, 160, 44),  
              (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
              (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
              (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
@@ -65,7 +64,7 @@ for i in range(len(tableau20)):
     r, g, b = tableau20[i]    
     tableau20[i] = (r / 255., g / 255., b / 255.)
 
-plt.figure(figsize=(12, 14))    
+plt.figure(figsize=(12, 8))    
 
 ax = plt.subplot(111)    
 ax.spines["top"].set_visible(False)    
@@ -76,28 +75,37 @@ ax.spines["left"].set_visible(False)
 ax.get_xaxis().tick_bottom()    
 ax.get_yaxis().tick_left()
 
-plt.ylim(0, 75)    
+plt.ylim(-5, 75)    
 plt.xlim(2005.5, 2019)
 
+plt.tick_params(axis="both", which="both", bottom="off",
+                labelbottom="on", left="off", labelleft="on")
 
-plt.yticks(range(0, 71, 10), [str(x) + "%" for x in range(0, 91, 10)], fontsize=14)    
+plt.yticks(range(0, 71, 10), [str(x) + "%" for x in range(0, 71, 10)], fontsize=14)    
 plt.xticks(fontsize=14)
 
-
-for y in range(10, 91, 10):    
-    plt.plot(range(2005, 2019), [y] * len(range(2005, 2019)), "--", lw=0.5, color="black", alpha=0.3)
-
-plt.tick_params(axis="both", which="both", bottom="off", top="off",    
-                labelbottom="on", left="off", right="off", labelleft="on")
+for y in range(10, 71, 10):    
+    plt.plot(range(2006, 2019), [y] * len(range(2006, 2019)), "--", lw=0.5, color="black", alpha=0.3)
 
 categories = ['RV', 'Tent', 'Camper', 'Watercraft', 'Car']
 
-for rank, column in enumerate(categories):    
-    # Plot each line separately with its own color, using the Tableau 20    
-    # color set in order.    
+for rank, column in enumerate(categories):       
     plt.plot(data.Year.values,    
             data[column.replace("\n", " ")].values,    
             lw=2.5, color=tableau20[rank])
-
+    
+    y_pos = data[column.replace("\n", " ")].values[-1] - 0.5
+    if column == 'Watercraft':    
+        y_pos += 1
+    elif column == 'Car':    
+        y_pos -= .25
+    elif column == 'Camper':    
+        y_pos -= 1.5
+    elif column == 'Tent':    
+        y_pos -= 1.5
+    elif column == 'RV':    
+        y_pos += 1.5
+    plt.text(2018.15, y_pos, column, fontsize=16, color=tableau20[rank])    
+plt.title('Reservation Type over Time', loc = 'right', fontsize = 20)
 plt.tight_layout()
 plt.savefig(imagepath + 'categories.png')
