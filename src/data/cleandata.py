0446810a90934986b2a8befdf56416c5ca045d78
hfeiss <hfeiss@gmail.com>
2020-01-08 09:39:18 -0700

    Clean cleandata.py

--- src/data/cleandata.py
+++ src/data/cleandata.py
@@ -28,106 +28,14 @@ def make_pkls(sourcepath, operation, years = None):
         print(f'Wrote {str(year[:-4])} in {df.folder}')
 
 if __name__ == '__main__':
-    list_res = []
-    for root, dirs, file in os.walk(respath):
-        list_res.extend(file)
-    list_res.sort()
-    #make this a function!!!!
-    '''
-    for year in list_res:
-        df = Data(respath + year)
-        print(f'{str(year)} pre:  {df.to_df().count()}')
-        df.clean()
-        print(f'{str(year)} post: {df.to_df().count()}')
-        df.make_CountsOfNights()
-        df.write_to_pkl(cleanpath
-                        + 'CountsOfNights/'
-                        + year[:-4] 
-                        + '.pkl')
-        print(f'Wrote {str(year[:-4])}')
 
-    '''
-    '''
-    for year in list_res:
-        df = Data(respath + year)
-        df.clean()
-        df.make_SumOfCategories()
-        df.write_to_pkl(cleanpath
-                        + 'SumOfCategories/'
-                        + year[:-4] 
-                        + '.pkl')
-        print(f'Wrote {str(year[:-4])}')
-    '''
-    '''
-    for year in list_res:
-        df = Data(respath + year)
-        df.clean()
-        df.make_DistanceByWeekend()
-        df.write_to_pkl(cleanpath
-                        + 'DistanceByWeekend/'
-                        + year[:-4] 
-                        + 'NoAK.pkl')
-        print(f'Wrote {str(year[:-4])}')
-
-    for year in list_res:
-        df = Data(respath + year)
-        df.clean()
-        df.make_DistanceByLonger()
-        df.write_to_pkl(cleanpath
-                        + 'DistanceByLonger/'
-                        + year[:-4] 
-                        + 'NoAK.pkl')
-        print(f'Wrote {str(year[:-4])}')
-    '''    
-    '''
-    for year in list_res:
-        df = Data(respath + year)
-        df.clean()
-        df.make_DistanceByCustomerZIP()
-        df.write_to_pkl(cleanpath
-                        + 'DistanceByCustomerZIP/'
-                        + year[:-4] 
-                        + '.pkl')
-        print(f'Wrote {str(year[:-4])}')
-    '''
-    '''
-    for year in list_res:
-        df = Data(respath + year)
-        df.clean()
-        df.make_DistanceByFacilityZIP()
-        df.write_to_pkl(cleanpath
-                        + 'DistanceByFacilityZIP/'
-                        + year[:-4] 
-                        + '.pkl')
-        print(f'Wrote {str(year[:-4])}')
-    '''
-    '''
-    for year in list_res:
-        df = Data(respath + year)
-        df.clean()
-        df.make_DistanceByFacilityState()
-        df.write_to_pkl(cleanpath
-                        + 'DistanceByFacilityState/'
-                        + year[:-4] 
-                        + '.pkl')
-        print(f'Wrote {str(year[:-4])}')
-    '''
-    list_res = ['2011.csv', '2012.csv', '2013.csv', '2014.csv', '2015.csv', '2016.csv', '2017.csv', '2018.csv']
-    
-    make_pkls(respath, Data.make_DistanceByCustomerState, list_res)
-    '''
-    for year in list_res:
-        df = Data(respath + year)
-        df.clean()
-        df.make_DistanceByCustomerState()
-        df.write_to_pkl(cleanpath
-                        + 'DistanceByCustomerState/'
-                        + year[:-4] 
-                        + '.pkl')
-        print(f'Wrote {str(year[:-4])}')
-    '''
-    '''
-    #lst = [data2006.df, data2007.df]
-    #data0607 = combine(*lst)
-    #print(f'Post combi: {data0607.count()}')
-    '''
+    # This is about 72 hours of computation for all years
+    years_left = ['2012.csv', '2013.csv', '2014.csv', '2015.csv', '2016.csv', '2017.csv', '2018.csv']
+    make_pkls(respath, Data.make_DistanceByCustomerState, years = years_left)
+    #make_pkls(respath, Data.make_DistanceByCustomerZIP)
+    #make_pkls(respath, Data.make_DistanceByFacilityState)
+    #make_pkls(respath, Data.make_DistanceByFacilityZIP)
+    #make_pkls(respath, Data.make_DistanceByWeekend)
+    #make_pkls(respath, Data.make_DistanceByLonger)
+    #make_pkls(respath, Data.make_SumOfCategories)
+    #make_pkls(respath, Data.make_CountsOfNights)
