a40720c23ce56747eeb6f3e2d4ba6bd2c6f4cf87
hfeiss <hfeiss@gmail.com>
2020-01-09 20:34:04 -0700

    Slow down gifs

--- src/visualizations/make_gif.py
+++ src/visualizations/make_gif.py
@@ -26,7 +26,7 @@ def make_gif(sourcepath, folder, years=None):
                    format='GIF',
                    append_images=frames[1:],
                    save_all=True,
-                   duration=600,
+                   duration=800,
                    loop=0)
 
 
